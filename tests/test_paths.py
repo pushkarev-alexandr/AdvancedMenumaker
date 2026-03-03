28667d94b04dce77d3638fe1fbe1d612e364b572
Pushkarev Aleksandr <pushkarev_alexandr@mail.ru>
2026-03-03 10:45:35 +0300

    Rename AdvancedMenumaker tests to test_paths and import normalizePath from new paths module

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -1,4 +1,4 @@
-from AdvancedMenumaker import normalizePath
+from advanced_menumaker.paths import normalizePath
 
 def test_normalizePath():
     assert normalizePath('../../gizmos') == 'Z:\\Nuke_Workgroup\\gizmos'
